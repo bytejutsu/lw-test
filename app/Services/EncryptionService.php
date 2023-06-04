cfc7e5492d8853d1aa511b93b44860666c6e04e2
dhia nabli <dhianabli@outlook.com>
2023-06-05 01:33:49 +0200

    implemented the EncryptionService

--- app/Services/EncryptionService.php
+++ app/Services/EncryptionService.php
@@ -4,8 +4,29 @@ namespace App\Services;
 
 class EncryptionService
 {
-    public function encryptCesar(string $input ,int $offset): string
+
+    private function cipher(string $ch, int $key): string
     {
-        return '';
+        if (!ctype_alpha($ch))
+            return $ch;
+
+        $offset = ord(ctype_upper($ch) ? 'A' : 'a');
+        return chr(fmod(((ord($ch) + $key) - $offset), 26) + $offset);
+    }
+
+    public function encrypt(string $input, int $key): string
+    {
+        $output = "";
+
+        $inputArr = str_split($input);
+        foreach ($inputArr as $ch)
+            $output .= $this->cipher($ch, $key);
+
+        return $output;
+    }
+
+    public function decrypt(string $input, int $key): string
+    {
+        return $this->encrypt($input, 26 - $key);
     }
 }
