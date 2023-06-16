2734d61e5f4f7b968253ba4b5e8e658350a2a840
dhia nabli <dhianabli@outlook.com>
2023-06-16 02:27:00 +0200

    made the services stats in the parent-component visually as a separate component

--- resources/views/livewire/parent-component.blade.php
+++ resources/views/livewire/parent-component.blade.php
@@ -7,43 +7,45 @@
 
     <br/>
 
+    <div class="border-4 border-purple-500 p-4 m-4">
+        <div class="grid grid-cols-3 divide-x-4 divide-purple-500 gap-4">
+            <div class="">
+                <div class="w-fit bg-blue-500 text-white text-sm font-bold p-2 ">Encryption Service</div>
+                <p><span class="text-blue-500">ABook Title:</span> {{ $this->aBookTitle }}</p>
+                <p><span class="text-blue-500">ABook Author:</span> {{ $aBook['author'] }}</p>
+                <hr/>
+                <p>ABook title letter count : <span class="text-blue-500 font-bold">{{ $this->aBookTitleLetterCount }}</span></p>
+                <hr/>
+            </div>
+            <div class="pl-4">
+                <div class="w-fit bg-red-500 text-white text-sm font-bold p-2 ">Database Service</div>
+                <p><span class="text-red-500">EBook Title: </span>{{ $eBook['title'] }}</p>
+                <p><span class="text-red-500">EBook Author: </span>{{ $eBook['author'] }}</p>
+                <hr/>
+                <p>EBook title letter count : <span class="text-red-500 font-bold">{{ $this->eBookTitleLetterCount }}</span></p>
+                <hr/>
+            </div>
+            <div class="pl-4">
+                <div class="w-fit bg-green-500 text-white text-sm font-bold p-2 ">API Service</div>
+                <p><span class="text-green-500">Product Title: </span>@if(isset($product)){{ $product->title }}@else Loading Product ... @endif</p>
+                <p><span class="text-green-500">Product Author: </span>@if(isset($product)){{ $product->author }}@else Loading Product ... @endif</p>
+                <hr/>
+                <p>Product title letter count : <span class="text-green-500 font-bold">@if(isset($product)){{ $this->productTitleLetterCount }}@else Loading Book ... @endif </span></p>
+                <hr/>
+            </div>
+        </div>
+    </div>
+
     <div class="flex flex-row justify-between">
-        <div class="basis-1/2 flex flex-col">
-            <div class="w-fit bg-blue-500 text-white text-sm font-bold p-2 ">Encryption Service</div>
-            <p><span class="text-blue-500">ABook Title:</span> {{ $this->aBookTitle }}</p>
-            <p><span class="text-blue-500">ABook Author:</span> {{ $aBook['author'] }}</p>
-
-            <hr/>
-
-            <p>ABook title letter count : <span class="text-blue-500 font-bold">{{ $this->aBookTitleLetterCount }}</span></p>
-
-            <hr/>
-
-            <br/>
-
-            <div class="w-fit bg-red-500 text-white text-sm font-bold p-2 ">Database Service</div>
-            <p><span class="text-red-500">EBook Title: </span>{{ $eBook['title'] }}</p>
-            <p><span class="text-red-500">EBook Author: </span>{{ $eBook['author'] }}</p>
-
-            <hr/>
-
-            <p>EBook title letter count : <span class="text-red-500 font-bold">{{ $this->eBookTitleLetterCount }}</span></p>
-
-            <hr/>
-
-            <br/>
-
-            <div class="w-fit bg-green-500 text-white text-sm font-bold p-2 ">API Service</div>
-
-            <p><span class="text-green-500">Product Title: </span>@if(isset($product)){{ $product->title }}@else Loading Product ... @endif</p>
-            <p><span class="text-green-500">Product Author: </span>@if(isset($product)){{ $product->author }}@else Loading Product ... @endif</p>
-            <hr/>
-            <p>Product title letter count : <span class="text-green-500 font-bold">@if(isset($product)){{ $this->productTitleLetterCount }}@else Loading Book ... @endif </span></p>
-
-            <hr/>
+        <div class="basis-1/3 flex flex-col">
+            here comes the map
         </div>
 
-        <div class="basis-1/2 flex flex-col justify-start items-stretch">
+        <div class="basis-1/3 flex flex-col">
+            here comes the coordinates list
+        </div>
+
+        <div class="basis-1/3 flex flex-col justify-start items-stretch">
             <livewire:product-list-component/>
         </div>
     </div>
